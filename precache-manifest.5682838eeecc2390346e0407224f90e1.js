499f50e28387006fa110be2aeeb33312537b7c1c
mmichle <manish.in@outlook.com>
2019-06-11 15:19:21 +0000

    Updates

---
+++ precache-manifest.5682838eeecc2390346e0407224f90e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3a58793c4eaebb999a1b55a1893c2f",
+    "url": "/Mmichle/ReactAppCRUD.git/index.html"
+  },
+  {
+    "revision": "4fab9beb7ba1c97e802a",
+    "url": "/Mmichle/ReactAppCRUD.git/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "609637d649fbdf27206c",
+    "url": "/Mmichle/ReactAppCRUD.git/static/js/2.74462cb6.chunk.js"
+  },
+  {
+    "revision": "4fab9beb7ba1c97e802a",
+    "url": "/Mmichle/ReactAppCRUD.git/static/js/main.4ba813d3.chunk.js"
+  },
+  {
+    "revision": "451294be9e15d52fd205",
+    "url": "/Mmichle/ReactAppCRUD.git/static/js/runtime~main.35f2419a.js"
+  }
+]);
